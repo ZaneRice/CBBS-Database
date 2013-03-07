4ae106015b59a7beb68d514a0188584630835413
Zane Thomas Rice <zaner@hornet22.cs.clemson.edu>
2013-03-07 01:07:53 -0500

    The script is getting pretty close to where we need it to be. The generation of the data needs to be abstracted to a function so that we can query multiples tables without too much trouble.

--- Database/PHP/exportDatabase.php
+++ Database/PHP/exportDatabase.php
@@ -3,13 +3,58 @@
 <body>
 
 <?php
-$output = fopen("output","w+");
+$database = mysqli_connect("","","","");
+
+// Check connection
+if (mysqli_connect_errno($database))
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
 $mentorTableName = "\"Mentor\"\n";
-$mentorColumns = "\"Email\" ,\"First\",\"Middle\",\"Last\" ,\"Work Phone\",\"Work Phone Ext.\" ,\"Mobile Phone\",\"Street Address\",\"City\" ,\"State\",\"Country\",\"Zip\",\"IsMentor\",\"IsMentee\",\"IsAdmin\" ,\"IsSponsor\" ,\"Years In Current Position\" ,\"Years In Company\" ,\"Company Name\" ,\"Position\" ,\"ShowInMatchResult\" ,\"Title\" ,\"Background\"\n";
+$mentorColumns = "";
+$mentorValues = "";
 
 /* TODO Query database for Mentor data */
-$mentorData = "\n";
+$mentorQuery = mysqli_query($database,"SELECT * FROM Mentor");
+$mentorValuesArray = array();
+$mentorValues = "";
+
+$row = mysqli_fetch_array($mentorQuery);
+$mentorKeys = array_keys($row);
+
+//Get the column names
+for($i = 1; $i < count($row); $i=$i + 2)
+{
+    $column = $mentorKeys[$i];
+
+    if ($mentorColumns === "")
+    {
+	$mentorColumns = "\"$column\""; 
+    }
+    else
+    {
+	$mentorColumns = $mentorColumns . ",\"$column\""; 
+    }
+}
+$mentorColumns = $mentorColumns . "\n";
+
+//Extract the row information
+do
+{
+    for($i = 0; $i < count($row)/2; $i = $i + 1)
+    {
+	if($i === 0)
+	{
+	    $mentorValues = $mentorValues . "\"" . $row[$i] . "\"";
+	}
+	else
+	{
+	    $mentorValues = $mentorValues . ",\"" . $row[$i] . "\"";
+	}
+    }
+    $mentorValues = $mentorValues . "\n";
+}while($row = mysqli_fetch_array($mentorQuery));
 
 $menteeTableName = "\"Mentee\"\n";
 $menteeColumns = "\"Email\", \"University\", \"Graduation Year\", \"Major\", \"First\", \"Middle\", \"Last\", \"Mobile Phone\", \"Street Address\", \"City\", \"State\", \"Country\", \"Zip\", \"IsMentor\", \"IsMentee\", \"IsAdmin\", \"ShowInMatchResult\", \"Background\"\n";
@@ -17,9 +62,12 @@ $menteeColumns = "\"Email\", \"University\", \"Graduation Year\", \"Major\", \"F
 /* TODO Query database for Mentee data */
 $menteeData = "\n";
 
-$toWrite = $mentorTableName . $mentorColumns . $mentorData . $menteeTableName . $menteeColumns . $menteeData;
-fwrite($output,$toWrite);
+mysqli_close($database);
 
+/* Write the output to a Excel readable file */
+$output = fopen("output","w+");
+$toWrite = $mentorTableName . $mentorColumns . $mentorValues;// . $menteeTableName . $menteeColumns . $menteeData;
+fwrite($output,$toWrite);
 fclose($output);
 ?>
 
