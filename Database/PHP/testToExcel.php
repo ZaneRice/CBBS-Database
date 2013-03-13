71a29a6a8cd1102e8d8fcf5dfcca0ed4cdce0373
Zane Rice <zanetrice@hotmail.com>
2013-03-13 16:38:01 -0400

    Adding a script we can use to test the toExcel function (as well as any of the other functions in excelFunctions.php) more robustly. Feel free to try and break the code. If we break it and fix it maybe the user will not.

---
+++ Database/PHP/testToExcel.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * Creates a file named testTables.csv which can be read
+ * in by Excel.
+ *
+ * The file is formatted to be human-readable and will contain the
+ * data from all tables listed in $tableNames
+ */
+require 'excelFunctions.php';
+
+//$database = mysqli_connect("","","","");
+$database = mysqli_connect("oss-ci.cs.clemson.edu","cpsc472","myDB4dmin","cpsc472");
+
+/* 
+ * The names of the tables which need to be written in an Excel
+ * readable format.
+ */
+$tableNames = array("Mentor", "Mentee");
+
+/*
+ * The name of the file which will contain the table data in an Excel
+ * readable format
+ */
+$filename = "testTables.csv";
+
+// Check connection
+if (mysqli_connect_errno($database))
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$toWrite = "";
+
+for ($i = 0; $i < count($tableNames); $i++)
+{
+    $tableName = $tableNames[$i];
+    $query = "SELECT * FROM $tableName WHERE IsMentor = 1";
+    $label = $tableName;
+    $toWrite = $toWrite . toExcel($database,$query,$label);
+}
+
+for ($i = 0; $i < count($tableNames); $i++)
+{
+    $tableName = $tableNames[$i];
+    $query = "SELECT * FROM $tableName WHERE IsMentor = 0";
+    $label = $tableName;
+    $toWrite = $toWrite . toExcel($database,$query,$label);
+}
+
+mysqli_close($database);
+
+/* Write the output to an Excel readable file */
+$outputFile = fopen($filename,"w+");
+fwrite($outputFile,$toWrite);
+fclose($outputFile);
+?>
