6d8632d2a84e4196216568a7f315475c02254e01
Zane Rice <zanetrice@hotmail.com>
2013-04-21 21:28:53 -0400

    Helps if I add the stuff I want to push

---
+++ Database/Demo/demo.php
@@ -0,0 +1,31 @@
+<?php
+require '../includes.php';
+
+function clear($database)
+{
+    $mentor = new Mentor;
+    $mentee = new Mentee;
+
+    //First clear Mentor and Mentee tables in the database
+    $mentor->clear($database);
+    $mentee->clear($database);
+}
+
+function export($database)
+{
+    exportTablesToExcel($database, 
+	Array("Mentor","Mentee"),
+	"databaseTable.csv");
+}
+
+function import($database)
+{
+    importDatabase($database,"databaseTables.csv");
+}
+
+$database = mysqli_connect("oss-ci.cs.clemson.edu",
+    "cpsc472","myDB4dmin","cpsc472");
+clear($database);
+export($database);
+//import($database);
+?>
